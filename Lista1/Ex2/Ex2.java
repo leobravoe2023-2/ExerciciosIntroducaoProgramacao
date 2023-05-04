9f2703481f98174ae7411af0bd0d9e1b6a6ca15c
leobravoe2023-2 <leonardo.estacio.gdrive2023-2@gmail.com>
2023-05-04 09:29:41 -0300

    Ajuste no Ex1 e Ex2 da Lista1

--- Lista1/Ex2/Ex2.java
+++ Lista1/Ex2/Ex2.java
@@ -5,10 +5,10 @@ public class SomaDoisNumeros {
         int numero1, numero2, resultado;
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Digite o primeiro número: ");
+        System.out.print("Digite o primeiro número:\n");
         numero1 = input.nextInt();
 
-        System.out.print("Digite o segundo número: ");
+        System.out.print("Digite o segundo número:\n");
         numero2 = input.nextInt();
 
         resultado = numero1 + numero2;
